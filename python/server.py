27775f6362d6686620dfeb2bc185247549821443
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2016-08-23 01:03:17 +0200

    Fix bug

--- python/server.py
+++ python/server.py
@@ -8,7 +8,7 @@ DATA_FILE = '../temps.csv'
 
 def main():
     if os.path.isfile(DATA_FILE):
-        df = pd.read_csv(DATA_FILE)
+        df = pd.read_csv(DATA_FILE, index_col=0)
     else:
         df = pd.DataFrame()
 
